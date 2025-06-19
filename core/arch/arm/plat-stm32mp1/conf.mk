6db07db4b8e8dbe10d94959280c797323cc34666
aravarum <aravindhanat7@gmail.com>
2025-06-19 23:24:11 +0530

    modified:   core/arch/arm/plat-stm32mp1/conf.mk 	modified:   ta/pkcs11/src/processing.h
    minor change

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -394,7 +394,7 @@ endif
 # Default enable some test facitilites
 CFG_ENABLE_EMBEDDED_TESTS ?= y
 CFG_WITH_STATS ?= y
-CFG_WERROR ?= y
+CFG_WERROR ?= n
 
 # Enable OTP update with BSEC driver
 CFG_STM32_BSEC_WRITE ?= y
